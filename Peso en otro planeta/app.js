f1ab99e77e82995ce9d4482245589ebae11bc562
Hustavo Jhon <HustavoJhon@gmail.com>
2022-06-26 20:13:57 -0700

    added folder Peso en otrs planetas

---
+++ Peso en otro planeta/app.js
@@ -0,0 +1,19 @@
+// mi peso
+var user = parseInt(prompt("Cual es tu peso?"));
+var planeta = parseInt(prompt("Elige tu planeta\n1 es marte y 2 es jupiter"));
+
+// gravedad en los diferentes planetas
+var g_tierra = 9.8;
+var g_marte = 3.7;
+var g_jupiter = 24.8;
+
+// condicional
+if(planeta == 1){
+    peso_marte = parseInt(user * g_marte / g_tierra);
+}
+document.write(`Tu peso en Marte es <strong>${peso_marte} Kilos </strong>`);
+
+// Respuesta
+var respuesta = parseInt(peso * g_marte / g_tierra);
+
+document.write("Tu p")
