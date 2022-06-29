e97f9faea941f901c2712b1b5a454eb0ade083d9
Hustavo Jhon <HustavoJhon@gmail.com>
2022-06-28 17:05:04 -0700

    add content a folder peso en otro plante

--- Peso en otro planeta/app.js
+++ Peso en otro planeta/app.js
@@ -1,9 +1,10 @@
-var usuario = prompt("Cuanto Pesas?");
-var peso = parseInt(usuario);
+// peso de el usuario
+var usuario = parseInt(prompt("Cuanto Pesas?"));
 
-var planeta_choice = prompt("Escoge tu planeta \n1=►Marte\n2=►venus\n3=►jupiter");
+// menu
+var menu = parseInt(prompt("Escoje tu planeta: \n1:Mercurio\n2:Venus\n3:Tierra\n4:Marte\n5:Jupiter\n6:Saturno\n7:Urano\n8:Nepturno"))
 
-var planet = planet_choice;
+var planet = planeta_choice;
 var g_mercurio = 3.7;
 var g_venus = 8.83;
 var g_tierra = 9.8;
@@ -13,11 +14,10 @@ var g_saturno = 10.44;
 var g_urano = 8.83;
 var g_nepturno = 11.15;
 
-switch (planeta) {
-    case 'Marte': case 'marte':
-        var g_plante = g_marte
-    break;
+if (planeta == 1){
+    peso_final = peso * g_mercurio / g_tierra;
+} else if(planeta == 2) {
+    peso_final = peso * g_venus / g_tierra;
+} else if
 
-    default:
-        break;
-}
+document.write("Tu en " + name_planeta + " pesa <strong>" + peso + "kilos </strong>")
