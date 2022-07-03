c5c75a14cd9a9dc8761bc620484781a1c4ce1efa
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-03 12:28:56 -0700

    added content folder 6

---
+++ javascript/6 Funciones/introduccion.js
@@ -0,0 +1,24 @@
+//! Introduccion
+
+//? Declaracion de Funcion
+// function name(parametros) { <code> }
+
+// Example 1
+function saluda() {
+    console.log("Hola");
+}
+saluda();
+
+// Example 2
+function cuadrado(numero) {
+    return numero * numero;
+    console.log("hola"); //TODO: no se ba a ejecutar ya que esta despues de return
+}
+let cuadrado_de_dos = cuadrado(2);
+console.log(cuadrado_de_dos); 
+console.log(cuadrado(3));
+
+//? Expresion de Funcion
+// let func = function(parametro) { <code> }
+
+MINUTO: 6:40
