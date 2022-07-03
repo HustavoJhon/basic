fb3d3f26f8da8d677453d1072fc665e1e647a0db
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-03 11:28:01 -0700

    added content folder 1

---
+++ javascript/1 Tipos de variables/undefined-Null-Nan.js
@@ -0,0 +1,19 @@
+//! UNDEFINED
+/*
+es un valor no definido como una variable sin nungun dato
+*/
+
+
+//! NULL
+/*
+es un valor que se puede asignar pero que esta vacio
+el tipo de una variable con null es un "Objeto"
+*/
+
+//! NAN
+/*
+? not a number
+es un valor que nos da para saber que no se pueden sumar o hacer una operacion aritmetica de un numero con un string
+
+10 ** "hola" ► nan
+*/
