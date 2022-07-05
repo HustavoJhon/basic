5411f6863b303080264daa2e47591137a2f9b4fd
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-04 22:21:26 -0700

    mas content

---
+++ javascript/6 Funciones/funciones.js
@@ -0,0 +1,19 @@
+//* ejercice 1
+// function mensaje(msj) {
+//     // console.log(msj);
+//     return "mensaje como argunmento"
+// }
+
+// var msj = mensaje();
+// console.log(msj);
+
+//* ejercice 2 
+// function sumar(a,b,c=3) {
+//     return a + b + c;
+// }
+// var result = sumar(4,5,8); //17
+// var result = sumar(3,7); //13
+// console.log(result);
+// console.log(result);
+
+//* ejercice 3 - Recursivas
