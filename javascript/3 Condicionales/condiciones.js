2183cdbef57d682e212f5df5b5478c594026e19f
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-04 18:05:56 -0700

    added ejercice a sentencias  y condicionales

--- javascript/3 Condicionales/condiciones.js
+++ javascript/3 Condicionales/condiciones.js
@@ -1,32 +1,109 @@
 //! IF
 
 //? en una sola linea
-if (10) console.log("10 es truthy"); //10 es truthy
-if (false) 
-    console.log("hola"); //no va a ejecutar nada por que la condicion es false
+// if (10) console.log("10 es truthy"); //10 es truthy
+// if (false) 
+//     console.log("hola"); //no va a ejecutar nada por que la condicion es false
 
 //? en blocke
-if (10 === '10') {
-    console.log("hola");
-}
+// if (10 === '10') {
+//     console.log("hola");
+// }
 
 //! ELSE
-let edad = 20;
-if (edad >= 18) {
-    console.log("Eres mayor de edad");
-} else {
-    console.log("Eres menor de edad");
-}
+// let edad = 20;
+// if (edad >= 18) {
+//     console.log("Eres mayor de edad");
+// } else {
+//     console.log("Eres menor de edad");
+// }
 
 //! ELSE IF
-let calificacion = 9;
+// let calificacion = 9;
 
-if (calificacion == 10) {
-    console.log("Exelente");
-} else if (calificacion > 7) {
-    console.log("Muy Bien");
-} else if (calificacion > 5){
-    console.log("Puedes Mejorar");
-} else{
-    console.log("reprobaste")
+// if (calificacion == 10) {
+//     console.log("Exelente");
+// } else if (calificacion > 7) {
+//     console.log("Muy Bien");
+// } else if (calificacion > 5){
+//     console.log("Puedes Mejorar");
+// } else{
+//     console.log("reprobaste")
+// }
+
+//* ejercice 1
+// var nombre = "Grover";
+// var esCasado = false;
+
+// if (esCasado == true) {
+//     console.log(nombre + " es casado");
+// } else {
+//     console.log(nombre + " es soltero")
+// }
+
+//* ejercice 2 - if anidados
+// var nombre ="Lucas";
+// var edad = 61;
+/*
+    edad < 12 es un niño
+    edad > 11, es < 18, es un adolescente
+    edad > 17, es < 60, es un adulto
+    edad > 60 es un anciano
+*/
+// if (edad < 12){
+//     console.log(nombre + ' es un niño');
+// } else if ((edad > 11) && (edad < 18)){
+//     console.log(nombre + ' es un adolescente');
+// } else if ((edad > 17) && (edad < 60)){
+//     console.log(nombre + ' es un adulto');
+// } else if (edad > 60){
+//     console.log(nombre + ' es un anciano');
+// } else{
+//     console.log("tu edad no tiene sentido")
+// }
+
+//* ejercice 3 - SWITCH
+var mes = 1;
+
+switch (mes) {
+    case 1:
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("Febrero");
+        break
+    case 3:
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("Abril");
+        break
+    case 5:
+        console.log("Mayo");
+        break
+    case 6:
+        console.log("Junio");
+        break;
+    case 7:
+        console.log("Julio");
+        break
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Septiempre");
+        break
+    case 10:
+        console.log("Octubree");
+        break;
+    case 11:
+        console.log("Noviembre");
+        break
+    case 12:
+        console.log("Diciembre");
+        break;
+
+    default:
+        console.log("mes incorrecto")
+        break;
 }
--- javascript/5 Ciclos/ciclos.js
+++ javascript/5 Ciclos/ciclos.js
@@ -8,18 +8,19 @@
 
 console.log("*************FOR***************");
 //! FOR
+//? for ([expresion-inicial]; [condicion]; [expresion-final])sentencia
 // Imprimir numeros del 1 al 10=
-for (let i = 1; i < 11; i++ ){ //or i = i + 1
-    console.log(i);
-}
+// for (let i = 1; i < 11; i++ ){ //or i = i + 1
+//     console.log(i);
+// }
 
 console.log("*************WHILE***************");
 //! WHILE
-let number = 1;
-while (number <= 10) {
-    console.log(number);
-    number++;
-}
+// let number = 1;
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
 
 // while (prompt()) {
 //     console.log("ejecucion");
@@ -27,24 +28,50 @@ while (number <= 10) {
 
 console.log("*************DO WHILE***************");
 //! DO WHILE
-do {
-    console.log("menu")
-} while (number == 3);
+// do {
+//     console.log("menu")
+// } while (number == 3);
 
 console.log("************BREAK****************");
 //! Break
-// para cualquier programa
-for (let i = 1; i < 11; i++ ){ 
-    console.log(i);
-    if (i == 5) {break;}
-}
+//? para cualquier programa
+// for (let i = 1; i < 11; i++ ){ 
+//     console.log(i);
+//     if (i == 5) {break;}
+// }
 
 console.log("*************CONTINUE***************");
 //! Continue
 // salta de la iteracion en la que vamos
-for (let i = 1; i < 11; i++ ){ 
-    console.log(i);
-    if (i % 2 != 0) {continue;}
+// for (let i = 1; i < 11; i++ ){ 
+//     console.log(i);
+//     if (i % 2 != 0) {continue;}
 
-    console.log("Es par")
-}
+//     console.log("Es par")
+// }
+
+
+
+//* ejercice 1 - FOR 
+// imprimir del 10 al 1
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);   
+// }
+//      del 0 al 50 de 5 en 5
+// for (let n = 0; n <= 50; n+=5){
+//     console.log(n);
+// }
+
+//* ejercice 2 - WHILE
+// var i = 10;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
+
+//* ejercice 3 - DO WHILE
+var i = 1;
+do{
+    console.log(i);
+    i++;
+} while(i <= 10);
