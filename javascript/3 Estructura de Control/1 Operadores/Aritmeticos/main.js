b9453418725d78cc923d736d29bf49d29cd4d033
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-12 16:42:10 -0700

    Operadores JS

---
+++ javascript/3 Estructura de Control/1 Operadores/Aritmeticos/main.js
@@ -0,0 +1,7 @@
+//! Aritmeticos
+
+let a = 5 + 5 - 10 * 3
+console.log(a)
+
+let modulo = 5%2; //residuo de la 
+console.log(modulo)
---
+++ javascript/3 Estructura de Control/1 Operadores/Booleanos/Relacionales/main.js
@@ -0,0 +1,16 @@
+//! RELACIONALES
+
+console.log(8 > 9);
+
+
+//? ahora usar el doble igual (==) es una mala practica
+
+
+// operadores unarios
+let i = 2;
+
+// imprime el valor y despues le suma 
+console.log(i++); //2
+
+// incrementa el valor y despues lo imprime
+console.log(++i); //4
