56809ff337384978f0b65d96b371d4d7cc503315
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-12 23:11:15 -0700

    Condicionales ternaio js

---
+++ javascript/3 Estructura de Control/2 Condicionales/main.js
@@ -0,0 +1,13 @@
+let edad = 18;
+// if (edad >= 18) {
+//     console.log("Eres mayor de edad")    
+// } else {
+//     console.log("Eres menor de edad")
+// }
+
+//! if else
+// Operdor tenario (condicional) ? verdadero:falsa
+// console.log("Operador Ternario");
+
+let ternario = (edad >= 18) ? "Eres Mayor de Edad" : "Eres menor de edad";
+console.log(ternario6)
