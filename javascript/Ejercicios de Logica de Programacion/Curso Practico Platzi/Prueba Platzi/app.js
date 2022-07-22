ff9e34126b02d08c159ae09506910c521a6d81b8
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-22 15:50:03 -0700

    test JS

---
+++ javascript/Ejercicios de Logica de Programacion/Curso Practico Platzi/Prueba Platzi/app.js
@@ -0,0 +1,14 @@
+//* Funciones
+const name = "Juan David";
+const lastname = "Castro Gallego";
+const completeName = name + lastname;
+const nickname = "juandc";
+function saludar(name=completeName,nick=nickname) {
+    console.log(`Mi nombre es ${completeName}, pero prefiero que me digas ${nickname}`);
+}
+saludar();
+
+//* Condicionales
+// Replica el comportamiento de tu condicional anterior con if, else y else if, pero ahora solo con if (sin else ni else if).
+//?💡 Bonus: si ya eres una experta o experto en el lenguaje, te desafío a comentar cómo replicar este comportamiento con arrays u objetos y un solo condicional. 
+
