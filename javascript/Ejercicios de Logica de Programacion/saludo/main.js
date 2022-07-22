ff4ff0c8df6e23342d876c2b239e1efe25f39c2b
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-21 22:49:12 -0700

    loops JS ejercice

--- javascript/Ejercicios de Logica de Programacion/saludo/main.js
+++ javascript/Ejercicios de Logica de Programacion/saludo/main.js
@@ -1,17 +1,45 @@
 // const name = document.getElementById('name');
 
-function saludar() {
-    const inputName = document.getElementById('username');
-    const nameuser = inputName.value;
-    alert(`Hola ${nameuser}`);
+// function saludar() {
+//     const inputName = document.getElementById('username');
+//     const nameuser = inputName.value;
+//     alert(`Hola ${nameuser}`);
+// }
+
+// function saludarAll() {
+//     const inputName = document.getElementById('username');
+//     const nameuser = inputName.value;
+
+//     const inputFriend = document.getElementById('friendname');
+//     const nameFriend = inputFriend.value;
+
+//     alert(`${nameuser} te saluda ${nameFriend}`);
+// }
+console.log("****************************");
+// ! FOR
+var estudiantes = ["maria","sergio","rosa","daniel"];
+
+function saludar(estudiantes) {
+    console.log(`Hola ${estudiantes}`);
 }
 
-function saludarAll() {
-    const inputName = document.getElementById('username');
-    const nameuser = inputName.value;
-
-    const inputFriend = document.getElementById('friendname');
-    const nameFriend = inputFriend.value;
-
-    alert(`${nameuser} te saluda ${nameFriend}`);
+for (var i = 0; i < estudiantes.length; i++) {
+    saludar(estudiantes[i]);
 }
+
+console.log("****************************");
+
+// ! FOR of
+for(var estudent of estudiantes) {
+    saludar(estudent);
+}
+console.log("****************************");
+//! while
+while (estudiantes.length > 0) {
+    var estudiante = estudiantes.shift();
+    saludar(estudiante);
+}
+
+console.log("****************************");
+
+console.log("****************************");
