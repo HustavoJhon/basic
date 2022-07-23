29a28d8e540dd7bc91e8ef95057ed5cb0612938e
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-23 09:55:21 -0700

    Cuanto es 2 + 2 prueva

--- javascript/Ejercicios de Logica de Programacion/Curso Practico Platzi/Prueba Platzi/app.js
+++ javascript/Ejercicios de Logica de Programacion/Curso Practico Platzi/Prueba Platzi/app.js
@@ -12,3 +12,24 @@ saludar();
 // Replica el comportamiento de tu condicional anterior con if, else y else if, pero ahora solo con if (sin else ni else if).
 //?💡 Bonus: si ya eres una experta o experto en el lenguaje, te desafío a comentar cómo replicar este comportamiento con arrays u objetos y un solo condicional. 
 
+//Ciclos
+
+let contador = 0;
+while (contador < 5) {
+    console.log(`El valor es: ${contador}`);
+    contador++;
+}
+
+let i = 10;
+while (i >= 2) {
+    console.log("xd"+ i);
+    i--;
+}
+
+//Cuanto es 2 + 2
+let respuesta;
+
+while (respuesta != '4') {
+    let pregunta = prompt("Cuanto es 2 + 2");
+    respuesta = pregunta;
+}
