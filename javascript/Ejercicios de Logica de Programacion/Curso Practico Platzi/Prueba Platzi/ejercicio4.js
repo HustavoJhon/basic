c405f0ad634d485d87c8841ae833004980bd5d52
Hustavo Jhon <HustavoJhon@gmail.com>
2022-07-24 16:27:16 -0700

    Ejercicio Object a Array

---
+++ javascript/Ejercicios de Logica de Programacion/Curso Practico Platzi/Prueba Platzi/ejercicio4.js
@@ -0,0 +1,23 @@
+//const obj = {
+//    name: "lucas",
+//    age: 45,
+//    fruty: "apple"
+//}
+
+//function inprimir(obj) {
+//    const convercion = Object.values(obj);
+//    for (let i = 0; i < convercion.length; i++) {
+//        const element = convercion[i];
+//        console.log(element)
+//    }
+//}
+//console.log(inprimir());
+//
+
+const obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+console.log(Object.values(obj));
