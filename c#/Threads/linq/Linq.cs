fa97b029821f65b6c8956bbbb744f63f3e236626
Hustavo Jhon <HustavoJhon@gmail.com>
2022-09-12 21:53:13 -0700

    nada

---
+++ c#/Threads/linq/Linq.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Thread.Tasks;
+using System.Windows.Forms;
+
+namespace Linq{
+    public partial class Form1 : Form1 {
+        public Form1(){
+            Initiali
+        }
+    }
+}
