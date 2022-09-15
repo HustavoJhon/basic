fff1436ef48d2309488508b4e045cad045151c06
Hustavo <hustavoJhon@gmail.com>
2022-09-15 09:55:29 -0700

    example c#

---
+++ c#/example/HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+{
+    class Program{
+        static void Main(String[] args){            
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+}
