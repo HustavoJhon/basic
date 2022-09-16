8483071ab3b90298b75016494bd92c64c8e03c32
Hustavo <hustavoJhon@gmail.com>
2022-09-16 09:17:20 -0700

    add file c# singleton senati

---
+++ c#/SENATI/Tarea1/Singleton.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton {
+        private static Singleton instance = null;
+        public string mensaje = "";
+        protected Singleton() {
+            mensaje = "hola mundo";
+        }
+        public static Singleton Instance {
+            get {
+                if (instance == null) 
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+    }
+    
+}
