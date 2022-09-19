c2cdc8e19122bd700f5725ded22fc03c78104d01
Hustavo <hustavoJhon@gmail.com>
2022-09-19 09:19:41 -0700

    add tarea Singleton c#

---
+++ c#/Tarea1/Singleton/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "hola Marte";
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+        }
+    }
+}
