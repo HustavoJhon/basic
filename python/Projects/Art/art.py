ffa873e0474a36dc2e47a4694119a51bc98d4205
Hustavo <hustavoJhon@gmail.com>
2022-10-04 06:08:04 -0700

    art.py

--- python/Projects/Art/art.py
+++ python/Projects/Art/art.py
@@ -1,3 +1,15 @@
-from art import tprint
+from art import *
 
-tprint("Gitconin", font="tarty2")
+frase = art("Coffe")
+
+tprint(frase)
+
+print(frase)
+
+tprint(frase, font="tarty2")
+
+tprint(frase, font="bulbhead", chr_ignore=True) 
+
+print(frase) 
+
+tprint(frase, font="fancy4", decoration="random")
