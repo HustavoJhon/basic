f4c424b55c2553a221537a166e214d73ed93d0f4
Hustavo <hustavoJhon@gmail.com>
2022-10-04 22:32:02 -0700

    cowsay pip py

---
+++ python/Projects/Art/cowsay.py
@@ -0,0 +1,15 @@
+import cowsay 
+
+# cowsay.char_name
+# cowsay.chars
+# len(cowsay.chars)
+
+# cowsay.cow("Hello World")
+
+print(cowsay.get_output_string('trex','Hello (extinct) World'))
+
+cowsay.dragon('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit rhoncus nibh. Mauris mi mauris, molestie vel metus sit amet, aliquam vulputate nibh.')
+
+# cowsay --character pig Hello World
+
+# cowsay Hello World --character tux
