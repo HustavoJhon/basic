f9bcb7036fd2bba46e33fd2f1e435ce3f6e67a58
Hustavo <hustavoJhon@gmail.com>
2022-10-05 08:18:23 -0700

    password generator py

---
+++ python/Projects/Generador de Contraseñas/main.py
@@ -0,0 +1,27 @@
+from email import charset
+from itertools import chain
+import random 
+
+def new_password():
+
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    carcateres = MAYUS + MINUS + NUMS + CHARS
+    password = []
+
+    for i in range(15):
+        caracteres_random = random.choice(carcateres)
+        password.append(caracteres_random)
+
+    password = ''.join(password)
+    return password
+
+def run():
+    password = new_password()
+    print(f'You new password is {password}')
+
+if __name__ == "__main__":
+    run()
