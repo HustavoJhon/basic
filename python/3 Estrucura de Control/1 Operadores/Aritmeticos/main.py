3fb7f7245032cd17edd8becb9755acec161f4556
Hustavo <hustavoJhon@gmail.com>
2022-11-30 19:44:59 -0800

    logicos and operation py

---
+++ python/3 Estrucura de Control/1 Operadores/Aritmeticos/main.py
@@ -0,0 +1,24 @@
+'''
+P - parentesis
+E - exponentes
+M - multiplicacion
+D - division
+A - adicion
+S - sustraccion
+'''
+
+print(10 + 10)
+print(10- 5)
+print(10 * 10)
+print(10 / 2) # 5
+print(10 % 2) # 0
+print(10 / 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+
+
+
+print('Hola' + 'Mundo')
+print('Hola' * 3)
---
+++ python/3 Estrucura de Control/1 Operadores/Booleanos/main.py
@@ -0,0 +1,51 @@
+'''
+OPERADORES DE COMPARACION
+
+== es ifual
+!= es distinto
+<  es menor que
+<= es menor o igual que
+>  es mayor que
+>= es mayot o igual que
+'''
+
+
+# >
+print(7 > 3)
+print(7 < 3)
+print(7 > 7)
+
+# <
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+
+# >=
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+
+# <=
+print(2 <= 1)
+print(2 <= 1)
+print(2 <= 2)
+
+# ==
+print(6 == 6)
+print(6 == 5)
+
+# !=
+print(6 != 10)
+print(6 != 6)
+
+
+# * strings 
+
+print("Apple" == 'Apple')
+print('Manzana' == 'manzana')
+
+print('1' == 1)
+
+
+age = 18
+print(age >= 18)
