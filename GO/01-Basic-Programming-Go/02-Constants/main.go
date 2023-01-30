2b9896cae5e889c2f11d15f04dc606a5cbb85f53
Humberto Yupanqui <npmyupanqui@gmail.com>
2023-01-29 21:08:32 -0500

    Added variables and constants

---
+++ GO/01-Basic-Programming-Go/02-Constants/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaración de constantes
+
+	// Una constante, a diferencia  de una variable es una valor que nunca va ha cambiar en el tiempo.
+
+	// Forma 1 de declarar constantes
+	const pi float32 = 3.14
+
+	// Forma 2 de declarar constantes
+	const pi2 = 3.1415
+
+	fmt.Println("Pi:", pi)
+	fmt.Println("Pi2:", pi2)
+}
