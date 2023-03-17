496b6341c3012cca1e252535c3a830680ea9c5e2
Gustavo Jhon <hustavojhon@gmail.com>
2023-03-17 09:48:47 -0500

    map python/courses

--- PYTHON/Courses/Platzi/14_map.py
+++ PYTHON/Courses/Platzi/14_map.py
@@ -4,7 +4,17 @@ numbers_v2 = []
 for i in numbers:
     numbers_v2.append(i * 2)
 
-numbers_v3 = list(map(lambda i: i * 2, numbers))
+numbers_v3 = list(map(lambda i:i*2, numbers))
 
 print(numbers)
 print(numbers_v2)
+print(numbers_v3)
+
+
+n = [1,2,3,4,5,6]
+n2 = [5,6,7]
+
+print(n)
+print(n2)
+result = list(map(lambda x, y: x + y, n, n2))
+print(result)
