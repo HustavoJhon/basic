373c6c0292e77cce06a1d30d7c1e138a364e4ff5
Gustavo Jhon <hustavojhon@gmail.com>
2023-03-20 16:45:09 -0500

    filter file python

---
+++ PYTHON/Courses/Platzi/17_filter.py
@@ -0,0 +1,3 @@
+numbers = [1,2,3,4,5]
+new_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(new_numbers)
---
+++ PYTHON/Courses/Platzi/17_filter_dict.py
@@ -0,0 +1,36 @@
+matches = [
+  {
+    'home_team': 'Bolivia',
+    'away_team': 'Uruguay',
+    'home_team_score': 3,
+    'away_team_score': 1,
+    'home_team_result': 'Win'
+  },
+  {
+    'home_team': 'Brazil',
+    'away_team': 'Mexico',
+    'home_team_score': 1,
+    'away_team_score': 1,
+    'home_team_result': 'Draw'
+  },
+  {
+    'home_team': 'Ecuador',
+    'away_team': 'Venezuela',
+    'home_team_score': 5,
+    'away_team_score': 0,
+    'home_team_result': 'Win'
+  },
+]
+
+print(matches)
+
+new_list = list(filter(lambda item: item['home_team_result'] == 'Win', matches))
+
+print(new_list)
+print(len(new_list))
+
+print('---')
+
+nueva_lista  = list(filter(lambda x: x['home_team_score'] == 3, matches))
+
+print(nueva_lista)
