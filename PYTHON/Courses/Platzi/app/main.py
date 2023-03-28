5e69d36624f3a9ac0b2878b8cf36a434a41401e4
Gustavo Jhon <hustavojhon@gmail.com>
2023-03-28 11:55:55 -0500

    use if __name__ == '__main__': for python

--- PYTHON/Courses/Platzi/app/main.py
+++ PYTHON/Courses/Platzi/app/main.py
@@ -1,21 +1,27 @@
 import mod 
 import utils
 
-keys, values = mod.get_population()
-print(keys, values)
-
-
 data = [
-    {
-        'Country': 'Colombia',
-        'Population': 500
-    },
-    {
-        'Country': 'Bolivia',
-        'Population': 300
-    }
-]
-country = input('Type Country => ')
+        {
+            'Country': 'Colombia',
+            'Population': 500
+        },
+        {
+            'Country': 'Bolivia',
+            'Population': 300
+        }
+    ]
 
-result = utils.population_by_country(data, country)
-print(result)
+def run():
+    keys, values = mod.get_population()
+    print(keys, values)
+
+
+    
+    country = input('Type Country => ')
+
+    result = utils.population_by_country(data, country)
+    print(result)
+
+if __name__ == '__main__':
+    run()
