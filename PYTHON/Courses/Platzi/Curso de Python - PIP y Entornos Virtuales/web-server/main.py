d0b04d33d8086d07d53ff13dcee08d880df81543
Gustavo Jhon <hustavojhon@gmail.com>
2023-04-06 17:08:01 -0500

    pip -i fastapi and uvicorn for my web site with python

--- PYTHON/Courses/Platzi/Curso de Python - PIP y Entornos Virtuales/web-server/main.py
+++ PYTHON/Courses/Platzi/Curso de Python - PIP y Entornos Virtuales/web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    
 
 def run():
     store.get_categories()
