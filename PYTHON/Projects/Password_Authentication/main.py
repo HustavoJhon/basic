aa7b465b7705be9d9311102f744c62377464053a
Gustavo Jhon <hustavojhon@gmail.com>
2024-01-26 10:40:04 -0500

    project Password Authentication in python

---
+++ PYTHON/Projects/Password_Authentication/main.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"user": "root", "user1": "admin"}
+username = input("Enter Yourname : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
