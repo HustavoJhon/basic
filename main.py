432246178ba8737350f6518386ae46de44c64436
hustavojhon <hustavojhon@gmail.com>
2024-10-28 21:01:39 -0700

    update main.py

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ from rich.console import Console
 from rich.markdown import Markdown
 
 MARKDOWN = """
-# This is an h1
-
-Rich can do a pretty *decent* job of rendering markdown.
-
-1. This is a list item
-2. This is another list item
+# This is basic 
 """
 
 
