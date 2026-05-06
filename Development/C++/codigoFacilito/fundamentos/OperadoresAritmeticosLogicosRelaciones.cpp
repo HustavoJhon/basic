867d2883f405f5ee21832c0fd02436db35977578
hustavojhon <hustavojhon@gmail.com>
2026-05-06 10:24:38 -0700

    docs: learn Operators in cpp

--- Development/C++/codigoFacilito/fundamentos/OperadoresAritmeticosLogicosRelaciones.cpp
+++ Development/C++/codigoFacilito/fundamentos/OperadoresAritmeticosLogicosRelaciones.cpp
@@ -23,4 +23,9 @@
 #include <iostream>
 using namespace std;
 
-int main() { int a = 10, b = 5; }
+int main() {
+  int a = 10, b = 5;
+  cout << a + b << endl;             // suma
+  cout << (a > b) << endl;           // a es mayor que b : 1 (true)
+  cout << (a > 5 && b < 10) << endl; // ambas condiciones verdaderas : 1 (true)
+}
